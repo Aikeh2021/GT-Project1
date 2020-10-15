5e88fa3f3dd202618dc4a2d45e0519861fdda1ff
Dennis Francis <dnfrancis22@gmail.com>
2020-10-14 21:17:42 -0400

    Created api key and ajax call.

--- script.js
+++ script.js
@@ -5,3 +5,20 @@
 // API RESPONSES
 // APPLICATION LOGIC
 // STORAGE
+$(document).ready(function () {
+
+    var apiKey = "AIzaSyAWsdDOn6l8M1xX89rUG_KTlrn_uJ8lSYw";
+    var URL = "https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=" + apiKey +
+    "&part=snippet,contentDetails,statistics,status";
+
+
+    $.ajax({
+       url: URL,
+       method: "GET",
+     }).then(function (response) {
+       console.log(response);
+
+     });
+});
+
+
