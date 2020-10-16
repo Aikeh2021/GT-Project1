8016128103a4d46524941c8ebf670abf6aab2c75
Tara Martin <taramartin@Taras-MacBook-Pro.local>
2020-10-16 17:40:27 -0400

    added api and ajax for met

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-// console.log("oh hey!");
+$(document).ready(function () {
+  function getCollection(keyword, qty, callback) {
+    const searchAPI =
+      "https://collectionapi.metmuseum.org/public/collection/v1/search?q=" +
+      keyword;
+    let data = [];
+    //make first API call, get array of objectIDs from MET
+    $.ajax({
+      type: "GET",
+      url: searchAPI,
+    }).then(getObjects);
 
-// APPLICATION VARIABLES
-// DOM ELEMENTS
-// SET EVENT LISTENERS/HANDLERS
-// API CALLS
-// API RESPONSES
-// APPLICATION LOGIC
-// STORAGE
+   
+
+  // DOM ELEMENTS
+  // SET EVENT LISTENERS/HANDLERS
+  // API CALLS
+  // API RESPONSES
+  // APPLICATION LOGIC
+  // STORAGE
+});
