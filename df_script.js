fd06bf1f7af9ba4bf5141f66d30aad768c8adc91
Dennis Francis <dnfrancis22@gmail.com>
2020-10-19 20:39:38 -0400

    cleaned up some code and made notes in my js file.

--- df_script.js
+++ df_script.js
@@ -1,41 +1,42 @@
 $(document).ready(function () {
+  var apiKey = "AIzaSyAWsdDOn6l8M1xX89rUG_KTlrn_uJ8lSYw";
 
-    var apiKey = "AIzaSyAWsdDOn6l8M1xX89rUG_KTlrn_uJ8lSYw";
-
-    var video = ''
-
-    $("#ytForm").submit(function (event) {
-      event.preventDefault();
-
-      var search = $("#search-input").val();
-      console.log(search);
-
-      videoSearch(apiKey, search, 10);
-    });
-    
-    function videoSearch(key, search, maxResults) {
-
-      $("#generated-content").empty()
-
-      $.get(
-        "https://www.googleapis.com/youtube/v3/search?key=" +
-          key +
-          "&type=video&part=snippet&maxResults=" +
-          maxResults +
-          "&q=" +
-          search,
-        function (data) {
-          console.log(data);
-          data.items.forEach(item => {
-            video = `
-
-            <iframe width="420" height="315" src="https://www.youtube.com/embed/${item.id.videoId}" frameborder= "0" allowfullscreen></iframe>
-
-            `
-            $("#generated-content").append(video)
-          });
-        }
-      );
-    }
-
+  var video = "";
+  // once the form is submitted
+  $("#Form").submit(function (event) {
+    // we run prevent default to keep from refreshing the page.
+    event.preventDefault();
+    // this variable holds the input data from the user input.
+    var search = $("#search-input").val();
+    // here we call the video search function.
+    videoSearch(apiKey, search, 10);
   });
+  // this is the video search function.
+  function videoSearch(key, search, maxResults) {
+    $("#generated-content").empty();
+    // this variable holds the url.
+    var URL =
+      "https://www.googleapis.com/youtube/v3/search?key=" +
+      key +
+      "&type=video&part=snippet&maxResults=" +
+      maxResults +
+      "&q=" +
+      search;
+    // this is the ajax call to get the data from the api.
+    $.ajax({
+      url: URL,
+      method: "GET",
+    }).then(function (response) {
+      // this dynamicaly generates html to hold the video.
+      response.items.forEach((item) => {
+        video = `
+
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/${item.id.videoId}" frameborder= "0" allowfullscreen></iframe>
+
+        `;
+        // here we append the video to the screen.
+        $("#generated-content").append(video);
+      });
+    });
+  }
+});
