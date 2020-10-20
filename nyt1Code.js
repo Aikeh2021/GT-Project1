a91fb8d5834125e4d11576f4d08603db659c836a
Phlipper <marzullop73@gmail.com>
2020-10-20 17:25:22 -0400

    changes to html filw

--- nyt1Code.js
+++ nyt1Code.js
@@ -1,7 +1,5 @@
-
-//  * pulls information from the form and build the query URL
-//  * @returns {string} URL for NYT API based on form inputs
- 
+// * @returns {string} URL for NYT API based on form inputs
+//  */
 function buildQueryURL() {
   // queryURL is the url we'll use to query the API
   var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
@@ -47,13 +45,13 @@ function updatePage(NYTData) {
   // Get from the form the number of results to display
   // API doesn't have a "limit" parameter, so we have to do this ourselves
   var numArticles = $("#article-count").val();
-// Log the NYTData to console, where it will show up as an object
+
+  // Log the NYTData to console, where it will show up as an object
   console.log(NYTData);
   console.log("------------------------------------");
 
   // Loop through and build elements for the defined number of articles
-  for (var i = 0; i < numArticles; i++) 
-//   {
+  for (var i = 0; i < numArticles; i++) {
     // Get specific article info for current index
     var article = NYTData.response.docs[i];
 
@@ -65,7 +63,7 @@ function updatePage(NYTData) {
     $articleList.addClass("list-group");
 
     // Add the newly created element to the DOM
-    $("#generated-content").append($articleList);
+    $("#article-section").append($articleList);
 
     // If the article has a headline, log and append to $articleList
     var headline = article.headline;
@@ -111,20 +109,19 @@ function updatePage(NYTData) {
 
     // Append the article
     $articleList.append($articleListItem);
+  }
 }
-
 
 // Function to empty out the articles
 function clear() {
-  $("#generated-content").empty();
+  $("#article-section").empty();
 }
 
 // CLICK HANDLERS
 // ==========================================================
-$("#btn-three").on("click", function(event){
-  $("#generated-content").empty("");
+
 // .on("click") function associated with the Search Button
-// $("#run-search").on("click", function(event) {
+$("#run-search").on("click", function(event) {
   // This line allows us to take advantage of the HTML "submit" property
   // This way we can hit enter on the keyboard and it registers the search
   // (in addition to clicks). Prevents the page from reloading on form submit.
